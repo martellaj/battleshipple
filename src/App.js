95753f2b562a2bae36bbc64093aa763bccb0bf24
Joe Martella <martellaj@outlook.com>
2022-06-08 23:14:40 -0700

    link

--- src/App.js
+++ src/App.js
@@ -7,6 +7,19 @@ function App() {
     <div className="App">
       <Header />
       <Game />
+      <a
+        style={{
+          marginTop: "auto",
+          marginBottom: "24px",
+          color: "#038eca !important",
+          fontSize: "20px",
+        }}
+        href="https://twitter.com/battleshipple"
+        target="_blank"
+        rel="noreferrer"
+      >
+        @battleshipple
+      </a>
     </div>
   );
 }
