bebf82b6061c84ee9b09fad89a627399186e2350
Joe Martella <martellaj@outlook.com>
2022-06-10 11:37:31 -0700

    science

--- src/helpers/getShipCoords.js
+++ src/helpers/getShipCoords.js
@@ -1,63 +1,32 @@
 export default function getShipCoords(puzzleNumber) {
-  return puzzles[puzzleNumber - 1];
-}
+  const coords = [];
 
-const puzzles = [
-  // 6/9/2022 (#1)
-  [
-    {
-      x: 0,
-      y: 2,
-    },
-    {
-      x: 1,
-      y: 2,
-    },
-    {
-      x: 2,
-      y: 2,
-    },
-    {
-      x: 3,
-      y: 2,
-    },
-  ],
-  // 6/10/2022 (#2)
-  [
-    {
-      x: 2,
-      y: 1,
-    },
-    {
-      x: 2,
-      y: 2,
-    },
-    {
-      x: 2,
-      y: 3,
-    },
-    {
-      x: 2,
-      y: 4,
-    },
-  ],
-  // 6/11/2022 (#3)
-  [
-    {
-      x: 0,
-      y: 0,
-    },
-    {
-      x: 0,
-      y: 1,
-    },
-    {
-      x: 0,
-      y: 2,
-    },
-    {
-      x: 0,
-      y: 3,
-    },
-  ],
-];
+  const date = new Date();
+
+  const x = (puzzleNumber + date.getDay()) % 5;
+  const y = (puzzleNumber + date.getFullYear()) % 5;
+  coords.push({ x, y });
+
+  const orientation = (x + y) % 2 === 0 ? "horizontal" : "vertical";
+  if (orientation === "horizontal") {
+    // keep y the same and change x
+    if (x < 2) {
+      coords.push({ x: x + 1, y }, { x: x + 2, y }, { x: x + 3, y });
+    } else if (x > 3) {
+      coords.push({ x: x - 1, y }, { x: x - 2, y }, { x: x - 3, y });
+    } else {
+      coords.push({ x: 1, y }, { x: 0, y }, { x: 3, y });
+    }
+  } else {
+    // keep x the same and change y
+    if (y < 2) {
+      coords.push({ y: y + 1, x }, { y: y + 2, x }, { y: y + 3, x });
+    } else if (y > 3) {
+      coords.push({ y: y - 1, x }, { y: y - 2, x }, { y: y - 3, x });
+    } else {
+      coords.push({ y: 1, x }, { y: 0, x }, { y: 3, x });
+    }
+  }
+
+  return coords;
+}
